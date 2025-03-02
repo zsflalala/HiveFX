6334a7ed1476a73b63ecd63b841acdf739fd846e
zsflalala <73597361+zsflalala@users.noreply.github.com>
2025-03-02 23:58:18 +0800

    minor change

--- mylibrary/src/main/cpp/Texture2D.cpp
+++ mylibrary/src/main/cpp/Texture2D.cpp
@@ -87,7 +87,7 @@ CTexture2D* CTexture2D::loadTexture(AAssetManager *vAssetManager, const std::str
     AAsset* pAsset = AAssetManager_open(vAssetManager, vTexturePath.c_str(), AASSET_MODE_BUFFER);
     if (!pAsset)
     {
-        LOG_ERROR(hiveVG::TAG_KEYWORD::TEXTURE2D_TAG, "Failed to open asset: %s", vTexturePath.c_str());
+        LOG_ERROR(hiveVG::TAG_KEYWORD::TEXTURE2D_TAG, "Failed to open asset: %s. Try searching in mobile.", vTexturePath.c_str());
         return nullptr;
     }
 
@@ -168,6 +168,43 @@ CTexture2D* CTexture2D::loadTexture(AAssetManager *vAssetManager, const std::str
     }
     stbi_image_free(pImageData);
 
+    return new CTexture2D(TextureHandle);
+}
+
+CTexture2D *CTexture2D::loadTextureFromMobile(const std::string &vTexturePath)
+{
+    int Width, Height, Channels;
+    unsigned char* pData = stbi_load(vTexturePath.c_str(), &Width, &Height, &Channels, 0);
+    if (pData == nullptr)
+    {
+        LOG_ERROR(TAG_KEYWORD::TEXTURE2D_TAG, "Failed to load texture asset: [%s]", vTexturePath.c_str());
+        return nullptr;
+    }
+
+    GLint Format = GL_RGB;
+    if (Channels == 3) Format = GL_RGB;
+    else if (Channels == 4) Format = GL_RGBA;
+    else if (Channels == 1) Format = GL_RED;
+
+    GLuint TextureHandle;
+    glGenTextures(1, &TextureHandle);
+    glBindTexture(GL_TEXTURE_2D, TextureHandle);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, Format, Width, Height, 0, Format, GL_UNSIGNED_BYTE, pData);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    bool IsValid = (glIsTexture(TextureHandle) == GL_TRUE);
+    if (!IsValid)
+    {
+        LOG_ERROR(TAG_KEYWORD::TEXTURE2D_TAG, "Failed to create texture: [%s]", vTexturePath.c_str());
+        return nullptr;
+    }
+    stbi_image_free(pData);
     return new CTexture2D(TextureHandle);
 }
 
