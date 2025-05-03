be5906ed3cc66969b19eaea99f1116ecf86d549b
zsflalala <73597361+zsflalala@users.noreply.github.com>
2025-05-03 10:06:05 +0800

    update system test 023 rain demo for HUAWEI

---
+++ system_test023_channelmultiplexing/src/main/cpp/Renderer.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <EGL/egl.h>
+#include "Common.h"
+
+struct android_app;
+
+namespace hiveVG
+{
+    class CRenderer
+    {
+    public:
+        CRenderer(android_app *vApp);
+        ~CRenderer();
+
+        void renderScene();
+
+    private:
+        void __initRenderer();
+        void __updateRenderArea();
+
+        template<typename T>
+        void __deleteSafely(T*& vPointer);
+
+        int           m_WindowWidth   = -1;
+        int           m_WindowHeight  = -1;
+        android_app*  m_pApp          = nullptr;
+        EGLDisplay    m_Display       = EGL_NO_DISPLAY;
+        EGLSurface    m_Surface       = EGL_NO_SURFACE;
+        EGLContext    m_Context       = EGL_NO_CONTEXT;
+    };
+
+    template<typename T>
+    void CRenderer::__deleteSafely(T*& vPointer)
+    {
+        if (vPointer != nullptr)
+        {
+            delete vPointer;
+            vPointer = nullptr;
+        }
+    }
+}
