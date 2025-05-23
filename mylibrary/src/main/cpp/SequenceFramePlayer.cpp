358720c290228ecba063364f011c6ed6bbb034d8
longye-wwb <84922243+longye-wwb@users.noreply.github.com>
2025-05-23 22:47:04 +0800

    完善了23测试用例，完成缩减云图高度之后的播放

--- mylibrary/src/main/cpp/SequenceFramePlayer.cpp
+++ mylibrary/src/main/cpp/SequenceFramePlayer.cpp
@@ -318,6 +318,21 @@ void CSequenceFramePlayer::drawInterpolation(CScreenQuad *vQuad)
     m_pSequenceShaderProgram->setUniform("CurrentTexture", 0);
     m_pSequenceShaderProgram->setUniform("NextTexture", 1);
     m_pSequenceShaderProgram->setUniform("Factor", m_InterpolationFactor);
+    m_pSequenceShaderProgram->setUniform("Displacement", 0);
+    m_pSequenceShaderProgram->setUniform("CurrentChannel", m_CurrentChannel);
+    glActiveTexture(GL_TEXTURE0);
+    m_SeqTextures[m_CurrentTexture]->bindTexture();
+    glActiveTexture(GL_TEXTURE1);
+    m_SeqTextures[m_NextTexture]->bindTexture();
+    vQuad->bindAndDraw();
+}
+void CSequenceFramePlayer::drawDisplacementInterpolation(CScreenQuad *vQuad)
+{
+    assert(m_pSequenceShaderProgram != nullptr);
+    m_pSequenceShaderProgram->useProgram();
+    m_pSequenceShaderProgram->setUniform("CurrentTexture", 0);
+    m_pSequenceShaderProgram->setUniform("NextTexture", 1);
+    m_pSequenceShaderProgram->setUniform("Factor", m_InterpolationFactor);
     m_pSequenceShaderProgram->setUniform("Displacement", 0.01f);
     m_pSequenceShaderProgram->setUniform("CurrentChannel", m_CurrentChannel);
     glActiveTexture(GL_TEXTURE0);
--- mylibrary/src/main/cpp/SequenceFramePlayer.h
+++ mylibrary/src/main/cpp/SequenceFramePlayer.h
@@ -55,6 +55,7 @@ namespace hiveVG
         void draw(CScreenQuad *vQuad);
         void drawMultiChannelKTX(CScreenQuad *vQuad);
         void drawInterpolation(CScreenQuad *vQuad);
+        void drawDisplacementInterpolation(CScreenQuad *vQuad);
         void setRatioUniform();
 
     protected:
@@ -93,6 +94,5 @@ namespace hiveVG
         EPictureType::EPictureType m_TextureType = EPictureType::PNG;
         std::vector<CTexture2D *> m_SeqTextures;
         CShaderProgram *m_pSequenceShaderProgram = nullptr;
-
     };
 }
